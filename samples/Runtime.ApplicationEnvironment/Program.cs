bc51998a3e9bd1f424b8bd2a3add5dfed7a931a8
codeyu <codeyu@gmail.com>
2016-12-07 15:12:04 +0800

    init

---
+++ samples/Runtime.ApplicationEnvironment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.Runtime.InteropServices;
+namespace Runtime.ApplicationEnvironment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var environment = PlatformServices.Default.Application;
+            
+            Console.WriteLine("======================================================");
+            Console.WriteLine(RuntimeInformation.FrameworkDescription);
+            Console.WriteLine(RuntimeInformation.OSDescription);
+            Console.WriteLine(RuntimeInformation.IsOSPlatform(OSPlatform.OSX));
+            Console.WriteLine("ApplicationName: {0}", environment.ApplicationName);
+            Console.WriteLine("ApplicationBasePath: {0}", environment.ApplicationBasePath);
+            Console.WriteLine("TargetFramework: {0}", environment.RuntimeFramework);
+            Console.WriteLine("Version: {0}", environment.ApplicationVersion);
+            Console.WriteLine("======================================================");
+            Console.ReadLine();
+        }
+    }
+}
