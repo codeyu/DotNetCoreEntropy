dda8a3d12231bc95e2f01b1184b1f5fb5364009d
codeyu <codeyu@gmail.com>
2016-12-17 19:31:51 +0800

    add NativeInteroperability

---
+++ samples/NativeInteroperability/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NativeInteroperability {
+    public static class Program {
+
+        // Import the libc and define the method corresponding to the native function.
+        [DllImport("libSystem.dylib")]
+        private static extern int getpid();
+
+        public static void Main(string[] args){
+            // Invoke the function and get the process ID.
+            int pid = getpid();
+            Console.WriteLine(pid);
+        }
+    }
+}
